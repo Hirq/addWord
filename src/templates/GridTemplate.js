0e71903d71d92ee1c635b11e72b5bb33cdc9776e
hirq <czarkooo@gmail.com>
2021-09-25 12:27:52 +0200

    feat: set filter tag - max 3 filter, use if/else statement

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -63,13 +63,6 @@ const options = [
   { value: 'Work', label: 'Work', id: 7 }
 ]
 
-const options2 = ['Book', 'Video','Internet', 'Story', 'Diary', 'Job','Holiday', 'Work'];
-const optionsTest = ['Job', 'Work'];
-
-const table = ['1','2','3','4'];
-const table2 = ['1','3'];
-const filtry = [0,2,4];
-
 const GridTemplate = ({path, notes, countNotes, archiveNote, countArchiveNote, blogs }) => {
   const [visibleBar, setVisibleBar] = useState(false);
   const [withArchive, setWithArchive] = useState(false);
@@ -78,17 +71,12 @@ const GridTemplate = ({path, notes, countNotes, archiveNote, countArchiveNote, b
   const [filterOptions, setFilterOptions] = useState([]);
   const [change, setChange] = useState(false);
   const [lengthTagFilter, setLengthTagFilter] = useState(0);
-  
-  
-  
+
   const handleChangeTagSelect = (newValue, actionMeta) => {
     // console.log(newValue[newValue.length-1].value) // OK
     console.log(newValue);
     setChange(true);
-
     // setAddFilter(newValue.map((i) => i.value));
-
-
     // setSearchTag(searchTag.concat(addFilter));
     // setSearchTag(searchTag.concat((newValue[newValue.length-1].value)));
     // setSearchTag(newValue[newValue.length-1].value);
@@ -149,12 +137,16 @@ const GridTemplate = ({path, notes, countNotes, archiveNote, countArchiveNote, b
       )
     }
   }
-
   const determinePathSearch = (path) => {
     if (path === 'blog'){
       return(
         <>  
-          <StyledReactSelect value={filterOptions}  isMulti options={options} onChange={handleChangeTagSelect}/>
+          {(searchTag.length > 2) ? (
+            <StyledReactSelect value={filterOptions}  isMulti onChange={handleChangeTagSelect}/>
+          ) : (
+            <StyledReactSelect value={filterOptions}  isMulti options={options} onChange={handleChangeTagSelect}/>
+          )
+          }
           <Input search placeholder="Search tag" value={searchTag} onChange={handleSearchTag}/>
           <Input search placeholder="Search blog" value={searchName} onChange={handleSearchName}/>
         </>
@@ -190,14 +182,69 @@ const GridTemplate = ({path, notes, countNotes, archiveNote, countArchiveNote, b
           </StyledGrid>
         )
       }
+      if (lengthTagFilter === 1) {
+        return (
+          <StyledGrid>
+            {blogs
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[0])))))
+            .map(({ title, content, date, tag, id }) => (
+                <Card
+                id={id}
+                title={title}
+                content={content}
+                date={date}
+                tag={tag}
+                key={id}
+                path="blog"
+                />
+              ))}
+          </StyledGrid>
+        )
+      }
+      if (lengthTagFilter === 2) {
+        return (
+          <StyledGrid>
+            {blogs
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[0])))))
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[1])))))
+            .map(({ title, content, date, tag, id }) => (
+                <Card
+                id={id}
+                title={title}
+                content={content}
+                date={date}
+                tag={tag}
+                key={id}
+                path="blog"
+                />
+              ))}
+          </StyledGrid>
+        )
+      }  
+      if (lengthTagFilter === 3) {
+        return (
+          <StyledGrid>
+            {blogs
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[0])))))
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[1])))))
+            .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[2])))))
+            .map(({ title, content, date, tag, id }) => (
+                <Card
+                id={id}
+                title={title}
+                content={content}
+                date={date}
+                tag={tag}
+                key={id}
+                path="blog"
+                />
+              ))}
+          </StyledGrid>
+        )
+      }  
       else {
         <StyledGrid>
           {blogs
-          // .map((searchTag) => (
-          //   .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[0])))))
-          // ))
-          .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[0])))))
-          .filter(item => (item.tag.some((tags) => (tags.includes(searchTag[1])))))
           .map(({ title, content, date, tag, id }) => (
             <Card
             id={id}
@@ -211,96 +258,8 @@ const GridTemplate = ({path, notes, countNotes, archiveNote, countArchiveNote, b
           ))}
       </StyledGrid>
       }
-      return(
-        <StyledGrid>
-
-          <ul>
-          {optionsTest.filter(item => !searchTag.includes(item)).map((item) => (
-            <li>
-              {item}
-            </li>
-          ))}
-          </ul>
-
-          {/* {options2.filter(item => !searchTag.includes(item))} */}
-
-          {/* porównuje string do array - dlatego nie dziala - trzeba porownac array do array, lub znaleźć metode, która kazdego stringa przszuka po tablicy */}
-
-
-          {/* .filter(item => (item.title.toLowerCase().includes(searchName))) */}
-
-          {/* {blogs.filter(item => (item.tag.find((tags) => !tags.includes(searchTag)))).filter(item => (item.title.toLowerCase().includes(searchName))).map(({ title, content, date, tag, id }) => ( */}
-          
-          {/* { lengthTagFilter === 0 ? 
-          
-          {blogs.map(({ title, content, date, tag, id }) => (
-              <Card
-              id={id}
-              title={title}
-              content={content}
-              date={date}
-              tag={tag}
-              key={id}
-              path="blog"
-              />
-            ))}
-          : null
-
-          } */}
-
-ilosc filtorw - {lengthTagFilter}
-
-{searchTag.map((filter, i) => <h1>{searchTag[i]} ? </h1>)}
-
-
-
-          {blogs
-            // .filter(item =>  (item.tag.find((tags) => searchTag.map((filter, i) => (tags.includes(searchTag[i]))) )))
-
-            
-          // .searchTag.map((filter, i) => {filtera(item => (item.tag.find((tags) => (tags.includes(searchTag[0]))))) }   )}
-          
-        //  .filter(item => (item.tag.find((tags) => (tags.includes(searchTag[0])))))
-        //  .filter(item => (item.tag.find((tags) => (tags.includes(searchTag[1]))))) 
-
-          .map(({ title, content, date, tag, id }) => (
-            <Card
-            id={id}
-            title={title}
-            content={content}
-            date={date}
-            tag={tag}
-            key={id}
-            path="blog"
-            />
-          ))}
-
-            {/* ////////////////// */}
-          <ul>
-          map filter
-          {blogs.filter(item => (item.tag.every((tags) => tags.includes(searchTag)))).map(({ tag }) => (
-            <li>
-            {tag}
-            </li>
-          ))}
-          map filter
-          </ul>
-
-
-
-
-          {/* <ul>
-          {blogs.filter(item => (item.tag.find((tags) => tags.includes(searchTag)))).map(({ tag }) => (
-            <li> = {tag} =</li> 
-          ))}
-          </ul> */}
-
-          {searchTag}
-
-        </StyledGrid>
-      )
     }
-    if (path == 'note'){
+    if (path === 'note'){
       return(
         <>
           <Button onClick={hideArchive}> Archive </Button>
@@ -311,11 +270,13 @@ ilosc filtorw - {lengthTagFilter}
     }
   }
 
-
   useEffect(() => {
     if (change == true) {
       console.log(filterOptions)
-      setSearchTag(filterOptions.map((i) => i.value));
+      if (filterOptions.length <= 3) {
+        setSearchTag(filterOptions.map((i) => i.value));
+      }
+
       console.log(searchTag)
       setLengthTagFilter(filterOptions.length)
       console.log(lengthTagFilter)
